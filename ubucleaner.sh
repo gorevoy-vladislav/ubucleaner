500c89f13d1e6983e818683efc248226593a7620
Vladislav Gorevoy <gorevoy-vladislav@users.noreply.github.com>
2016-07-02 01:28:11 +0600

    Few new version of ubucleaner
    - Add "-auto" option;
    - Add cleaning with deborphan;
    - Deborphan don't working with "-auto" option;

--- ubucleaner.sh
+++ ubucleaner.sh
@@ -3,6 +3,9 @@
 # Andrew Pennebaker
 # 9 Mar 2011
 #
+# Gorevoy Vladislav
+# 2 Jul 2016
+#
 # From openDesktop
 # http://opendesktop.org/content/show.php/Ubucleaner?content=71529
 
@@ -16,16 +19,26 @@ RED="\033[0;31m"
 ENDCOLOR="\033[0m"
 
 if [ $USER != root ]; then
-  echo -e $RED"Error: must be root"
+  echo -e $RED"Error: You must be root"
   echo -e $YELLOW"Exiting..."$ENDCOLOR
   exit 0
 fi
 
 echo -e $YELLOW"Removing old config files..."$ENDCOLOR
-apt-get purge $OLDCONF
+
+if [ "$1" = "-auto" ]; then
+  apt-get purge -y $OLDCONF
+else
+  apt-get purge $OLDCONF
+fi
 
 echo -e $YELLOW"Removing old kernels..."$ENDCOLOR
-apt-get purge $OLDKERNELS
+
+if [ "$1" = "-auto" ]; then
+  apt-get purge -y $OLDKERNELS
+else
+  apt-get purge $OLDKERNELS
+fi
 
 echo -e $YELLOW"Cleaning apt cache..."$ENDCOLOR
 apt-get autoclean && apt-get clean
@@ -34,15 +47,33 @@ echo -e $YELLOW"Emptying trash..."$ENDCOLOR
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 
-echo -e $YELLOW"Autoremove old packet..."$ENDCOLOR
-apt-get autoremove
+echo -e $YELLOW"Autoremove old packets..."$ENDCOLOR
 
-echo -e $YELLOW"Cleaning with deborphan..."$ENDCOLOR
-while [ -n "`deborphan`" ]; do
-    deborphan
-    echo
-    apt-get purge `deborphan`
-done
+apt-get autoremove -y
+
+if [ "$1" != "-auto" ]; then
+
+  if which deborphan; then
+
+    echo -e $YELLOW"Cleaning with deborphan..."$ENDCOLOR
+
+    read -r -p "Are you sure you want to clean with Deborphan? [Y/n]" response
+    response=${response,,}
+    if [[ $response =~ ^(yes|y| ) ]]; then
+
+      while [ -n "`deborphan`" ]; do
+        deborphan
+        echo
+        apt-get purge 'deborphan'
+      done
+
+    fi
+
+  else echo -e $RED"Deborphan is not installed! If you want to clean with it, install it with: sudo apt-get install deborphan!"$ENDCOLOR
+
+  fi
+
+fi
 
 echo -e $YELLOW"Script Finished!"$ENDCOLOR
 df -h
